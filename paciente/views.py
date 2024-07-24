ad79f1206c811dae4169581790963cdd6ed4912a
Chawder7 <dev.hufa@gmail.com>
2024-07-24 13:57:52 -0600

    tabla de pacientes funcional

--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Paciente
+from receta.models import Receta
 
 # Create your views here.
 def pacientes(request):
-    return render(request, "paciente/viewPaciente.html")
+    pacientes =  Paciente.objects.annotate(num_recetas=Count('receta')).only("id","nombrePaciente","apellidoPaciente","edad","genero")
+    return render(request, "paciente/viewPaciente.html",{'pacientes':pacientes})
 
 def pacienteDetalles(request):
     return render(request, "paciente/viewPacienteInfo.html")
