35abe36012cdac030fdd6c7062181a7bd3bef0ef
HuguitoU <hugou770@gmail.com>
2024-07-24 21:44:51 -0600

    Vista y formulario de pacientes funcional

--- paciente/views.py
+++ paciente/views.py
@@ -3,6 +3,8 @@ from django.db.models import Q, Count
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Paciente
 from receta.models import Receta
+from .forms import PacienteForm
+from django.contrib import messages
 
 # Create your views here.
 def pacientes(request):
@@ -28,3 +30,23 @@ def pacientes(request):
 
 def pacienteDetalles(request):
     return render(request, "paciente/viewPacienteInfo.html")
+
+def formPaciente(request):
+    return render(request, "paciente/formPaciente.html")
+
+def registrarPaciente(request):
+    if request.method == 'POST':
+        formPac = PacienteForm(request.POST, request.FILES)
+        if formPac.is_valid():
+            nombrePaciente = request.POST['nombrePaciente']
+            apellidoPaciente = request.POST['apellidoPaciente']
+            edad = request.POST['edad']
+            genero = request.POST['genero']
+            fotoPaciente = request.FILES['fotoPaciente']
+            insert = Paciente(nombrePaciente = nombrePaciente, apellidoPaciente = apellidoPaciente, edad = edad, genero = genero, fotoPaciente = fotoPaciente)
+            insert.save()
+            return render(request, "paciente/formPaciente.html")
+        else:
+            messages.error(request, "Error al procesar el formulario")
+    else: 
+        return render(request, "paciente/formPaciente.html", {'paciente': Paciente})
