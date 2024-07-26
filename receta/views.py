6964f75618f1299108f73be78cf5567ccf07f54e
Chawder7 <dev.hufa@gmail.com>
2024-07-25 18:44:35 -0600

    paginacion y tabla recetas

--- receta/views.py
+++ receta/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from receta.models import Receta
 
 # Create your views here.
 
 def viewReceta(request):
-    return render(request,"receta/viewRecetas.html")
+    query = request.GET.get('busqueda','')
+    recetas = Receta.objects.all()
+
+    if query:
+        recetas = recetas.filter(
+            Q(paciente__nombrePaciente__icontains=query) | 
+            Q(doctor__username__icontains=query)
+        )
+
+    paginacion = Paginator(recetas,8)
+    pagina = request.GET.get('page')
+    try:
+        recetas = paginacion.page(pagina)
+    except PageNotAnInteger:
+        recetas = paginacion.page(1)
+    except EmptyPage:
+        recetas = paginacion.page(paginacion.num_pages)
+
+    return render(request,"receta/viewRecetas.html",{'recetas':recetas, 'query':query})
 
 def recetaDetalles(request):
     return render(request, "receta/viewRecetasInfo.html")
